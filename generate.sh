798abe11f343831633a19aed01572854e43190cc
Evgastap <Evgastap@gmail.com>
2015-12-27 23:17:45 +0400

    minor changes

--- generate.sh
+++ generate.sh
@@ -12,11 +12,20 @@ time node generate.js
 echo "\nKilling static file server\n"
 kill $!
 
-
 #echo "Running merge-pdfs.sh...\n"
 #node merge-pdfs.js
 
 cd output
 cd pdf
 
+echo "Merging pdfs...\n"
 pdftk *.pdf cat output diary.pdf
+echo "Diary.pdf created in /output/pdf\n"
+
+mv Diary.pdf ../
+echo "Diary.pdf moved to the output directory\n"
+
+cd -
+mkdir -p html
+mv *.html html
+exit
